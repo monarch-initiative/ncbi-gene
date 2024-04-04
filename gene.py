f02aeee4bcfaac863a9a963ef50c8e7f1d8a9389
Kevin Schaper <kevinschaper@gmail.com>
2024-04-04 09:31:34 -0700

    Initial commit of a simple flat modular koza transform of ncbi gene into kgx format

---
+++ gene.py
@@ -0,0 +1,18 @@
+from koza.cli_runner import get_koza_app
+from biolink_model.datamodel.pydanticmodel_v2 import Gene
+
+koza_app = get_koza_app("ncbi_gene")
+
+while (row := koza_app.get_row()) is not None:
+
+    gene = Gene(
+        id='NCBIGene:' + row["GeneID"],
+        symbol=row["Symbol"],
+        name=row["Symbol"],
+        full_name=row["Full_name_from_nomenclature_authority"],
+        description=row["description"],
+        in_taxon=['NCBITaxon:' + row["tax_id"]],
+        provided_by=["infores:ncbi-gene"]
+    )
+
+    koza_app.write(gene)
